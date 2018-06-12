6f9e97b3137e164bb37049af89b5191b97205a0b
JeremyTCD <jeremytanchongdao@gmail.com>
2018-06-12 12:46:34 +0800

    Minor XML comment changes for PrismService

--- src/Prism/PrismService.cs
+++ src/Prism/PrismService.cs
@@ -28,7 +28,7 @@ namespace JeremyTCD.WebUtils.SyntaxHighlighters.Prism
         /// Highlights <paramref name="code"/>.
         /// </summary>
         /// <param name="code">Code to highlight.</param>
-        /// <param name="languageAlias">A Prism language alias. Visit https://prismjs.com/index.html#languages-list for a list of language aliases.</param>
+        /// <param name="languageAlias">A Prism language alias. Visit https://prismjs.com/index.html#languages-list for the list of valid language aliases.</param>
         /// <returns>Highlighted <paramref name="code"/>.</returns>
         /// <exception cref="ArgumentNullException">Thrown if <paramref name="code"/> is null.</exception>
         /// <exception cref="ArgumentException">Thrown if <paramref name="languageAlias"/> is not a valid Prism language alias.</exception>
@@ -69,7 +69,8 @@ namespace JeremyTCD.WebUtils.SyntaxHighlighters.Prism
         /// <summary>
         /// Returns true if <paramref name="languageAlias"/> is a valid Prism language alias. Otherwise, returns false.
         /// </summary>
-        /// <param name="languageAlias">Language alias to validate. Visit https://prismjs.com/index.html#languages-list for a list of language aliases.</param>
+        /// <param name="languageAlias">Language alias to validate. Visit https://prismjs.com/index.html#languages-list for the list of valid language aliases.</param>
+        /// <returns>true if <paramref name="languageAlias"/> is a valid Prism language alias. Otherwise, false.</returns>
         /// <exception cref="NodeInvocationException">Thrown if a Node error occurs.</exception>
         public virtual async Task<bool> IsValidLanguageAlias(string languageAlias)
         {
