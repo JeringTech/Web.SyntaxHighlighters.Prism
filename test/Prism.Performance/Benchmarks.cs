4170ce66bc3238893372d9a6d1df4d51cae28cd7
JeremyTCD <jeremytanchongdao@gmail.com>
2018-07-21 23:09:18 +0800

    Added Performance tests

---
+++ test/Prism.Performance/Benchmarks.cs
@@ -0,0 +1,41 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace JeremyTCD.WebUtils.SyntaxHighlighters.Prism.Performance
+{
+    [MemoryDiagnoser]
+    public class Benchmarks
+    {
+        private ServiceProvider _serviceProvider;
+        private int _counter;
+        private IPrismService _prismService;
+
+        [GlobalSetup(Target = nameof(PrismService_Highlight))]
+        public void NodeJSService_InvokeFromFile_Setup()
+        {
+            ServiceCollection services = new ServiceCollection();
+            services.AddPrism();
+            _serviceProvider = services.BuildServiceProvider();
+            _prismService = _serviceProvider.GetRequiredService<IPrismService>();
+            _counter = 0;
+        }
+
+        [Benchmark]
+        public async Task<string> PrismService_Highlight()
+        {
+            string result = await _prismService.HighlightAsync($@"function exampleFunction(arg) {{
+    // Example comment
+    return arg + '{_counter++}';
+}}",
+                "javascript");
+            return result;
+        }
+
+        [GlobalCleanup]
+        public void Cleanup()
+        {
+            _serviceProvider.Dispose();
+        }
+    }
+}
---
+++ test/Prism.Performance/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace JeremyTCD.WebUtils.SyntaxHighlighters.Prism.Performance
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchmarks>();
+        }
+    }
+}
